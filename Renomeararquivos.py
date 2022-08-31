a6e9e243310d5511ff9454fc231b920d17965864
Reginaldo Barbosa <112530481+Reginaldo-projects@users.noreply.github.com>
2022-08-31 01:11:14 -0300

    commit inicial

---
+++ Renomeararquivos.py
@@ -0,0 +1,15 @@
+import os
+pasta = str(input('Informe o caminho da pasta: ')+'\\')
+print(pasta)
+teste = 1
+for contagem in range(teste):
+        for caminho in os.listdir(pasta):
+            try:
+                        contagem = contagem + 1
+                        nomeantigo = pasta + caminho
+                        nomeatual = pasta  +str(contagem)+"-"+ caminho
+                        os.rename(nomeantigo,nomeatual)
+                        print(os.listdir(pasta))
+            except:
+                print('Ocorreu um Erro')
+
